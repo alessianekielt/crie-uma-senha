d6cc580f1d277818bf8e3e36600595b989264e10
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-05-27 18:24:13 -0300

    2705

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const numeroSenha = document.querySelector('.parametro-senha-texto');
 let tamanhoSenha = 12;
-senha.textContent = tamanhoSenha; 
-const botoes = document.querySelector('.parametro-senha-botao');
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha-botao'); 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
-   if(tamanhoSenha > 1){
-   // tamanhoSenha = tamanhoSenha - 1;
-   }
-   numeroSenha.textContent = tamanhoSenha;
-   geraSenha();
-}
-function aumentaTamanho(){
-    if(tamanhoSenha < 20){
-    // tamanhoSenha = tamanhoSenha + 1;
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
 // codigo omitido
 const campoSenha = document.querySelector('#campo-senha');
