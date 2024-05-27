2d141c364a40b310f298a7e240f7ba87e86fdd20
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-05-27 18:38:45 -0300

    2705

--- main.js
+++ main.js
@@ -42,7 +42,6 @@ function geraSenha(){
     if (checkbox[3].checked){
     alfabeto = alfabeto + simbolos;
     }
-    console.log(alfabeto);
     let senha = '';
     for (let i = 0; i < tamanhoSenha;i++){
     let numeroAleatorio = Math.random()*alfabeto.length;
