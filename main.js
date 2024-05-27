7763326cd4b9b2329f0b3b62734241436a9bd806
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-05-27 18:33:22 -0300

    2705

--- main.js
+++ main.js
@@ -27,10 +27,10 @@ const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 function geraSenha(){
     let senha = '';
-    for (let i = 0; i < tamanhoSenha; i++){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+    for (let i = 0; i < tamanhoSenha;i++){
+        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio);
+        senha = senha + letrasMaiusculas[numeroAleatorio];
     }
+    campoSenha.value = senha;
 }
-campoSenha.value = senha;
