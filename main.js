d06d35ad8be3c1ce976bb66d9af0e251c0c559ee
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-05-27 18:27:54 -0300

    2705

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ function diminuiTamanho(){
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
        // tamanhoSenha = tamanhoSenha+1;
-       tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
