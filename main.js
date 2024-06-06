b50a235233f6821efa781a7d5105245eb4867ec2
alessianekielt <159924142+alessianekielt@users.noreply.github.com>
2024-06-06 14:34:30 -0300

    0606

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ function geraSenha(){
     classificaSenha(alfabeto.length);
     }
 function classificaSenha(tamanhoAlfabeto){
-    let entropia = tamanhoSenha * Math.log2(alfabeto.length);
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     forcaSenha.classList.remove('fraca','media','forte');
     if (entropia > 57){
         forcaSenha.classList.add('forte');
@@ -64,4 +64,4 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
 }
-valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
+valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
